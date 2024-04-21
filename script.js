d252b3234530b678f2ce3f489bf7a7acb6181af7
altanurbalik <167699839+altanurbalik@users.noreply.github.com>
2024-04-21 19:07:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showPopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+    
+}
+
+function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+    
+}
